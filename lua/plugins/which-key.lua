8a4e335741a78f2f7ff52c6cb2481e8c217f0ec2
Allan Deutsch <allan@allandeutsch.com>
2024-08-06 00:08:03 -0400

    Added which-key groups for git and edgy panes

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,66 +1,76 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-    spec = {
-      {
-        -- press <leader>b to show a list of all buffers
-        "<leader>b",
-        group = "buffers",
-        expand = function()
-          return require("which-key.extras").expand.buf()
-        end,
-      },
-      { -- expands into a list of commands to delete specific buffers
-        "<leader>bd",
-        group = "delete buffer",
-        expand = function()
-          -- modified from https://github.com/folke/which-key.nvim/blob/6c1584eb76b55629702716995cca4ae2798a9cca/lua/which-key/extras.lua#L31-L46
-          -- to delete buffers instead of focusing hem
-          local wkextras = require("which-key.extras") -- for helper fns
-          local ret = {} ---@type wk.Spec[]
-          local buffers = vim.tbl_filter(function(buf) -- filter out all
-            return vim.bo[buf].buflisted          -- unlisted buffers
-          end, vim.api.nvim_list_bufs())          -- from the list
+	"folke/which-key.nvim",
+	event = "VeryLazy",
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+		spec = {
+			{
+				-- press <leader>b to show a list of all buffers
+				"<leader>b",
+				group = "buffers",
+				expand = function()
+					return require("which-key.extras").expand.buf()
+				end,
+			},
+			{ -- expands into a list of commands to delete specific buffers
+				"<leader>bd",
+				group = "delete buffer",
+				expand = function()
+					-- modified from https://github.com/folke/which-key.nvim/blob/6c1584eb76b55629702716995cca4ae2798a9cca/lua/which-key/extras.lua#L31-L46
+					-- to delete buffers instead of focusing hem
+					local wkextras = require("which-key.extras") -- for helper fns
+					local ret = {} ---@type wk.Spec[]
+					local buffers = vim.tbl_filter(function(buf) -- filter out all
+						return vim.bo[buf].buflisted -- unlisted buffers
+					end, vim.api.nvim_list_bufs()) -- from the list
 
-          for _, buf in ipairs(buffers) do
-            local name = wkextras.bufname(buf)
-            ret[#ret + 1] = {
-              "",
-              function()
-                vim.api.nvim_buf_delete(buf, {}) -- delete the buffer
-              end,
-              desc = name,                   -- set description to filename
-              icon = { cat = "file", name = name }, -- set icon based on filetype
-            }
-          end
-          return wkextras.add_keys(ret)
-        end,
-      },
-      {
-        "<leader>bda",
-        function()
-          local buffers = vim.api.nvim_list_bufs()
-          for _, buf in ipairs(buffers) do
-            if vim.api.nvim_get_current_buf() ~= buf then
-              vim.api.nvim_buf_delete(buf, {})
-            end
-          end
-        end,
-        desc = "Buffers: Delete All (others)",
-      },
-    },
-  },
-  keys = {
-    {
-      "<leader>?",
-      function()
-        require("which-key").show({ global = false })
-      end,
-      desc = "Buffer Local Keymaps (which-key)",
-    },
-  },
+					for _, buf in ipairs(buffers) do
+						local name = wkextras.bufname(buf)
+						ret[#ret + 1] = {
+							"",
+							function()
+								vim.api.nvim_buf_delete(buf, {}) -- delete the buffer
+							end,
+							desc = name, -- set description to filename
+							icon = { cat = "file", name = name }, -- set icon based on filetype
+						}
+					end
+					return wkextras.add_keys(ret)
+				end,
+			},
+			{
+				"<leader>bda",
+				function()
+					local buffers = vim.api.nvim_list_bufs()
+					for _, buf in ipairs(buffers) do
+						if vim.api.nvim_get_current_buf() ~= buf then
+							vim.api.nvim_buf_delete(buf, {})
+						end
+					end
+				end,
+				desc = "Buffers: Delete All (others)",
+			},
+			{
+				-- pane management hotkeys defined in edgy.lua
+				"<leader>p",
+				group = "Panes",
+			},
+			{
+				-- git hotkeys defined in git.lua
+				"<leader>g",
+				group = "Git",
+			},
+		},
+	},
+	keys = {
+		{
+			"<leader>?",
+			function()
+				require("which-key").show({ global = false })
+			end,
+			desc = "Buffer Local Keymaps (which-key)",
+		},
+	},
 }
